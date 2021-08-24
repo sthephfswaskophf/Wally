b3321bea76fb23fbf5b96f32db36a6db327fd42b
William Marques <75446300+sthephfswaskophf@users.noreply.github.com>
2021-08-24 11:29:29 -0300

    Delete wally/wally directory

--- wally/wally/devedor/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from devedor.models import Devedor
-
-class DevedorAdmin(admin.ModelAdmin):
-    list_display =('name', 'id_tax', 'id_espaider', 'contabil', 'risco', 'wallet_type', 'status_negociacao', 'user_agent',)
-    list_filter =('user_agent', 'status_negociacao')
-
-admin.site.register(Devedor, DevedorAdmin)
